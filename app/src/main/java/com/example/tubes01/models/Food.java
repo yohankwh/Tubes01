2961808ff60b0cf723df1699a8b61743a59bf9ae
Yohan Kurnia Wijaya <yohanhuang300999@gmail.com>
2020-10-13 22:31:18 +0700

    Test Commit

--- app/src/main/java/com/example/tubes01/models/Food.java
+++ app/src/main/java/com/example/tubes01/models/Food.java
@@ -7,5 +7,11 @@ public class Food {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
